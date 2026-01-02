56de44fdc5cd325422d4799e8012d50e10b0acbd
ww93fun <ww93fun@gmail.com>
2026-01-02 23:31:33 +0800

    feat: add vite-plugin-sitemap for improved site mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://ww93.com',
+      outDir: 'build',
+      dynamicRoutes: [
+        '/',
+        '/event-logger',
+        '/hanzi',
+        '/pinyin',
+        '/md2image',
+      ],
+    }),
+  ],
   build: {
     outDir: 'build',
   },
